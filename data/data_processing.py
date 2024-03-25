b74e9aad221f1452ec475c6b3ffcd62ae5e03ce3
IBS <125046407+ibelfis@users.noreply.github.com>
2024-03-25 19:30:58 +0100

    import df

--- data/data_processing.py
+++ data/data_processing.py
@@ -2,8 +2,18 @@ import pandas as pd
 
 df = pd.read_csv('data/raw_data.csv')
 
-selected_countries = df.loc[df['Country Code'].isin(["BEN","BFA","CAN","CHE","DEU","HTI","MLI","NOR","SEN"])]
 
-selected_countries_indicators = selected_countries.loc[selected_countries['Indicator Code'].isin(["PV.EST","GE.EST","IQ.CPA.FINQ.XQ","IQ.CPA.DEBT.XQ"])]
+country_list=["BEN","BFA"] #["BEN","BFA","CAN","CHE","DEU","HTI","MLI","NOR","SEN"]
+indic_list=["PV.EST","GE.EST"] #["PV.EST","GE.EST","IQ.CPA.FINQ.XQ","IQ.CPA.DEBT.XQ"]
 
-print(selected_countries_indicators.tail())
+selected_countries = df.loc[df['Country Code'].isin(country_list)]
+selected_countries_indicators = selected_countries.loc[selected_countries['Indicator Code'].isin(indic_list)]
+
+selected_countries_indicators = selected_countries_indicators.drop(['Country Name','Indicator Name'], axis=1)
+
+selected_countries_indicators = selected_countries_indicators.drop(selected_countries_indicators.columns[-1], axis=1)
+
+
+df2 = pd.melt(selected_countries_indicators, id_vars=['Country Code',"Indicator Code"], var_name='Date', value_name="Value")
+
+print(df2)
